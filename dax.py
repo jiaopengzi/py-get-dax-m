31f98fb50c942ec6348ef997a0ce3c1e130b45e3
jiaopengzi <jiaopengzi@outlook.com>
2023-03-09 18:48:34 +0800

    重构 补全中文信息及对应匹配

--- dax.py
+++ dax.py
@@ -9,180 +9,56 @@
 @Description    :   python 获取 dax 官方文档信息
 """
 import os
-import sys
-import json
-import copy
-from bs4 import element
-from bs4 import BeautifulSoup
-import requests
-import lxml
+
+from utils import Utils
 
 
 class DAX(object):
     """ DAX 官方文档获取为 json 文案 用于分析使用
     """
 
-    @staticmethod
-    def write_json_in_file(path: str, json_dic: dict) -> None:
-        """覆盖写入 json 文件
+    def get_dax(self) -> str:
+        """通过 json 清洗为 需要的字典
 
-        Args:
-            path (str):json文件的路径
-            json_dic (dict):需要写入的字典内容
-
-        Returns:None
-
+        :return: 文件路径
         """
-        with open(path, "w", encoding="utf8") as f:
-            f.write(json.dumps(json_dic, indent=4, ensure_ascii=False))
+        url_base_en = "https://learn.microsoft.com/en-us/dax/"
+        url_base_cn = "https://learn.microsoft.com/zh-cn/dax/"
+        url_en = "https://learn.microsoft.com/en-us/dax/toc.json"
+        url_cn = "https://learn.microsoft.com/zh-cn/dax/toc.json"
+        dic_en = Utils.response_json_to_dict(url_en)
+        dic_cn = Utils.response_json_to_dict(url_cn)
+        func_dict = {}
+        items_en = dic_en.get("items")[0].get("children")[1].get("children")
+        items_cn = dic_cn.get("items")[0].get("children")[1].get("children")
+        # print(items_cn)
+        # i = 0
+        for item_en, item_cn in zip(items_en[2:], items_cn[2:]):
 
-    @staticmethod
-    def base_dir() -> str:
-        """获取当前文件夹路径
-
-        Returns:
-                返回主文件文件夹绝对路径
-        """
-        if getattr(sys, "frozen", False):
-            return os.path.dirname(os.path.abspath(sys.executable))
-        return os.path.dirname(os.path.abspath(__file__))
-
-    @staticmethod
-    def get_dax_category(language: str, dic: dict = None) -> dict:
-        """获取 dax 函数信息类别
-
-        :param language: 对应函数版本 中文版:zh-cn 和 英文版:en-us
-        :param dic: 函数类别字典
-        :return: 返回函数类别字典
-        """
-
-        if dic is None:
-            dic = {}
-        url = f'https://learn.microsoft.com/{language}/dax/dax-function-reference'
-
-        try:
-            response = requests.get(url)
-            response.encoding = 'utf-8'
-        except:
-            raise Exception("请求失败")
-
-        html_content = response.text
-        bs = BeautifulSoup(html_content, "lxml")
-        class_div = {"class": "content"}
-        content = bs.find("div", class_div)
-
-        for item in content:
-            if item != '\n' and type(item) == element.Tag and item.a is not None:
-                herf = item.a["href"]
-                if herf not in dic:
-                    dic[herf] = {}
-                dic[herf][f"category-{language}"] = item.a.text
-
-        return dic
-
-    @staticmethod
-    def get_dax(category_dax: str, language: str, dic_func: dict = None, dic_category: dict = None) -> dict:
-        """获取 dax 函数信息
-
-        :param category_dax: 函数分类英文名称
-        :param language: 对应函数版本 中文版:zh-cn 和 英文版:en-us
-        :param dic_func: 函数字典
-        :param dic_category: 函数类别的字典，有就使用，没有不添加
-        :return:返回函数参数字典
-        """
-
-        global name
-        if dic_func is None:
-            dic_func = {}
-        url_base = f'https://learn.microsoft.com/{language}/dax/'
-        url = url_base + category_dax
-
-        try:
-            response = requests.get(url)
-            response.encoding = 'utf-8'
-        except:
-            raise Exception("请求失败")
-
-        html_content = response.text
-        bs = BeautifulSoup(html_content, "lxml")
-        table = bs.find("table")
-        rows = table.find_all("tr")
-
-        for row in rows[1:]:
-            cols = row.find_all("td")
-            for col in cols:
-                # ================================================单独处理一下中文版中 PATH 没有链接属性。
-                if col.text == "PATH":
-                    name = col.text
-                    # print(name)
-                    if name not in dic_func:
-                        dic_func[name] = {}
-                    dic_func[name][f"url-{language}"] = f"{url_base}path-function-dax"
-                # ================================================单独处理一下中文版中 PATH 没有链接属性。
-
-                if col.a is not None and col.text != "PATH":
-                    name = col.text
-                    # print(name)
-                    if name not in dic_func:
-                        dic_func[name] = {}
-                    dic_func[name][f"url-{language}"] = url_base + col.a["href"]
-                dic_func[name][f"description-{language}"] = col.text
-                if dic_category:
-                    dic_func[name][f"category-{language}"] = dic_category[category_dax][f"category-{language}"]
-
-        return dic_func
-
-    @staticmethod
-    def trim_dict(dic: dict, first_n: int, last_n: int) -> dict:
-        """切片字典，删除字典前几项和后几项目
-
-        :param dic: 字典
-        :param first_n: 要删除的前几个元素
-        :param last_n: 要删除的后几个元素
-        :return: 删除后的字典
-        """
-        # 删除前 first_n 元素
-        new_dic = copy.deepcopy(dic)
-        del_keys = list(new_dic.keys())[:first_n]
-        for key in del_keys:
-            del new_dic[key]
-
-        # 删除最后 last_n 个元素
-        del_keys = list(new_dic.keys())[-last_n:]
-        for key in del_keys:
-            del new_dic[key]
-
-        return new_dic
-
-    def get_all_functions(self) -> str:
-        """获取所有 DAX 函数信息
-
-        :return: 文件下载路径
-        """
-        # 中文分类
-        category_cn = self.get_dax_category("zh-cn")
-
-        # 英文分类，二参字典延续使用 category_cn
-        category_en = self.get_dax_category("en-us", category_cn)
-
-        # 区域第一个和最后一个不需要的类别
-        category = self.trim_dict(category_en, 1, 1)
-
-        all_func_dict = {}
-
-        # 通过类别循环获取函数信息
-        for c in category:
-            func_en = self.get_dax(category_dax=c, language="en-us", dic_category=category)
-            func_cn = self.get_dax(category_dax=c, language="zh-cn", dic_func=func_en, dic_category=category)
-            all_func_dict |= func_cn
-
-        # 加入 dax.guide 地址
-        for dax in all_func_dict:
-            all_func_dict[dax]["url-dax-guide"] = f"https://dax.guide/{dax.lower()}/"
+            category_en = item_en.get("toc_title")
+            category_cn = item_cn.get("toc_title")
+            group_en = item_en.get("children")
+            # print(group_en)
+            for fx in group_en[1:]:
+                fx_url_en = url_base_en + fx.get("href")
+                fx_url_cn = url_base_cn + fx.get("href")
+                # i += 1
+                # print(i)
+                # print(fx.get("toc_title"))
+                func_dict[fx.get("toc_title")] = {"url-en-us"         : fx_url_en,
+                                                  "url-zh-cn"         : fx_url_cn,
+                                                  "description-en-us" : Utils.get_func_description(fx_url_en),
+                                                  "category-en-us"    : category_en,
+                                                  "url-category-en-us": url_base_en + group_en[0].get("href"),
+                                                  "category-zh-cn"    : category_cn,
+                                                  "url-category-zh-cn": url_base_en + group_en[0].get("href"),
+                                                  "description-zh-cn" : Utils.get_func_description(fx_url_cn),
+                                                  "url-dax-guide"     : f'https://dax.guide/{fx.get("toc_title").lower()}/'
+                                                  }
 
         # 函数信息使用 json 存放在当前文件夹下
-        json_path = os.path.join(self.base_dir(), "dax.json")
-        self.write_json_in_file(json_path, all_func_dict)
+        json_path = os.path.join(Utils.base_dir(), "dax.json")
+        Utils.write_json_in_file(json_path, func_dict)
         return json_path
 
 
@@ -191,9 +67,8 @@ if __name__ == "__main__":
 
     start_time = time.time()
     dax = DAX()
-    download = dax.get_all_functions()
+
+    download = dax.get_dax()
     print(f"DAX 下载完毕！文件存放位置：{download}")
     end_time = time.time()
-    print(f"耗时 {(end_time - start_time):.2f} 秒！")
-
-    # 大约耗时 21.20 秒！
+    print(f"耗时 {(end_time - start_time):.2f} 秒！")  # 耗时 276.18 秒！,注意英文版需要科学上网
---
+++ main.py
@@ -0,0 +1,29 @@
+# -*- encoding: utf-8 -*-
+"""
+@File           :   main.py
+@Time           :   2023-03-09, 周四, 18:18
+@Author         :   焦棚子
+@Email          :   jiaopengzi@qq.com
+@Blog           :   https://jiaopengzi.com/
+@Version        :   1.0.0
+@Description    :   获取 dax & m 官方文档信息 入口文件
+"""
+
+import time
+
+from dax import DAX
+from m import M
+
+start_time_dax = time.time()
+dax = DAX()
+download_dax = dax.get_dax()
+print(f"DAX 下载完毕！文件存放位置：{download_dax}")
+end_time_dax = time.time()
+print(f"耗时 {(end_time_dax - start_time_dax):.2f} 秒！")  # 耗时 276.18 秒！,注意英文版需要科学上网
+
+start_time_m = time.time()
+m = M()
+download_m = m.get_m()
+print(f"Power Query 下载完毕！文件存放位置：{download_m}")
+end_time_m = time.time()
+print(f"耗时 {(end_time_m - start_time_m):.2f} 秒！")  # 耗时 386.98 秒！
